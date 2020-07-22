bf28a3f8a234e2a5866879700a3d704e37fe6ad6
eva-2021 <68548906+eva-2021@users.noreply.github.com>
2020-07-22 11:09:27 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+from aaa2 import *
+
+
+
+folyo.info()
+folyo.add(1500)
+folyo.update()
+folyo.info()
+
+
+folyo.add(900)
+folyo.update()
+folyo.info()
+
+
+
+print(inventory_amount)
+print(inventory_amount_new)
+
+print(len(inventory_amount))
+print(len(inventory_amount_new))
+
